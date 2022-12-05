cf85d1d79d82fdf9ce2b6bff8b0e1bc077467946
chengisucar <cengiz.ucar@billie.io>
2022-12-05 16:15:11 +0100

    update db connection data

--- app/dbconnect/conn.php
+++ app/dbconnect/conn.php
@@ -1,11 +1,13 @@
 <?php
 
+//  echo phpinfo();
+
 // connect to the database
-$conn = mysqli_connect('db', 'php_docker', 'password', 'php_docker');
+$conn = mysqli_connect('db', 'php_user', 'password', 'php_db');
 
 // check connection
 if(!$conn){
     echo 'Connection error: '. mysqli_connect_error();
-}
+} 
 
 ?>
